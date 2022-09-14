ee6f5da465d3556ba55ca71424881a8a72dcd336
roberthTR <104153702+roberthTR@users.noreply.github.com>
2022-09-14 13:45:05 -0500

    Tarea Var y Let

---
+++ clase del modulo/lib/js/main.js
@@ -0,0 +1,22 @@
+console.log("Desarrolo Web")
+console.log("Mary Stuart");
+console.log("1542");
+console.log(1542);
+console.log("abc", "def", "ghi");
+
+console.log("Mary Stuart (1542)");
+console.log("Mary", " Stuart ", "(1542)");
+
+
+console.log("Mary");
+console.log("Stuart ");
+console.log("(1542)");
+
+console.log("Mary" + " Stuart" + "(1542)");
+
+console.log("Mary " + " Stuart " + "(1542)");
+
+
+
+
+
---
+++ clase del modulo/lib/js/tarea.js
@@ -0,0 +1,86 @@
+    /****Tarea*****/
+    console.log("****Tarea*****");
+    //1. 10 variables tipo var
+    var name1;
+        //console.log(name1);
+    var lastname;
+    var titulo;
+    var profesion;
+    var sexo;
+    var sangre;
+    var contestura;
+    var saludo;
+    var dia;
+    var objeto;
+    var mascota;
+    var nameA;
+    //2. 10 variables tipo let
+    let edad;
+        //console.log(edad);
+    let año;
+    let DNI;
+    let añoNacimiento;
+    let numero;
+    let altura;
+    let peso;
+    let codigo;
+    let ciclo;
+    let IMC;
+    let mes;
+    let fechaingreso;
+    //3. asignar 10 variables con valores numéricos
+    edad=23;
+    año=2022;
+    DNI=73141714;
+    añoNacimiento=1999;
+    numero=912433994;
+    altura=1.69;
+    peso=68;
+    codigo=2019160036;
+    ciclo=5;
+    IMC=peso/Math.pow(altura);
+    mes=2;
+    fechaingreso=2019;
+
+    //4. asignar 10 variables con texto
+    name1="Roberth Jerson";
+    lastname="TORRES RIMAC";
+    titulo="Ing.";
+    profesion="Sistemas e Informatica";
+    sexo="Masculino";
+    sangre="O+";
+    contestura="Delgada";
+    saludo="Hola, Un gusto conocerte!";
+    dia="Lunes";
+    objeto="Celular";
+    mascota="Gato";
+    nameA="Michi";
+    //5. Mostrar todo en el depurador
+
+    console.log(codigo);
+    console.log(name1+", "+lastname);
+    console.log(edad);
+    console.log(DNI);
+    console.log(añoNacimiento);
+    console.log(sexo);
+    console.log(titulo);
+    console.log(profesion);
+    console.log(numero);
+    console.log(contestura);
+    console.log(altura);
+    console.log(peso);
+    console.log(saludo);
+    console.log(dia);
+    console.log(mes);
+    console.log(fechaingreso);
+    console.log(mascota);
+    console.log(nameA);
+    console.log(IMC);
+    
+
+    
+/*
+var  --> texto
+let --> numeros
+console.log() -->para mostrar igual que echo, printf
+*/
